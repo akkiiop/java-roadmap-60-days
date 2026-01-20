d8e80d32e61dccba05655c1b58a9a3cd842a9ca8
Akshay Kawade <kawadeakshay93@gmail.com>
2026-01-20 13:16:20 +0530

    Add day01-basics folder

---
+++ day01-basics/AssignmentOperatorDemo.java
@@ -0,0 +1,9 @@
+package day1;
+
+public class AssignmentOperatorDemo {
+    public static void main(String[] args) {
+        int y = 10;
+        y -= 5;
+        System.out.println("Result: " + y);
+    }
+}
