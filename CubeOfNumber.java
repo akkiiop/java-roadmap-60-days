1d530c0779884e44da3b0b9ca47a008c3cde4a9a
Akshay Kawade <kawadeakshay93@gmail.com>
2026-01-20 12:57:57 +0530

    Add Day 01 basics programs

---
+++ CubeOfNumber.java
@@ -0,0 +1,18 @@
+package day1;
+
+import java.util.Scanner;
+
+public class CubeOfNumber {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int num = sc.nextInt();
+
+        int cube = num * num * num;
+        System.out.println("Cube: " + cube);
+
+        sc.close();
+    }
+}
+
