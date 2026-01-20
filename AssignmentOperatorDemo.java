42a14283b393ef2be44df01699c066c01b9debcc
Akshay Kawade <kawadeakshay93@gmail.com>
2026-01-20 13:26:46 +0530

    Remove duplicate Day 01 java files from root

--- AssignmentOperatorDemo.java
+++
@@ -1,9 +0,0 @@
-package day1;
-
-public class AssignmentOperatorDemo {
-    public static void main(String[] args) {
-        int y = 10;
-        y -= 5;
-        System.out.println("Result: " + y);
-    }
-}
